bc195f84c45b32e6dc35406e875457c67e8f57b3
Jeramey Beck <jeramey.beck@atlbecks.com>
2018-03-23 11:11:33 -0400

    Initial Commit

---
+++ Get it There/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Get it There
+//
+//  Created by Jeramey Beck on 3/23/18.
+//  Copyright © 2018 Jeramey Beck. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
